cf70c968c6132327d7ac26f36e42b40ea83083fe
Luis Miguel <luismiguel@ubuntu.(none)>
2012-12-12 17:49:32 +0000

    first commit

---
+++ application/logs/2012-12-11.log.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-12-11 19:08:59 +00:00 --- error: core.uncaught_exception
---
+++ themes/default/views/main/map.php
@@ -0,0 +1,11 @@
+<!-- map -->
+<div class="map" id="map"></div>
+<div style="clear:both;"></div>
+<div id="mapStatus">
+	<div id="mapScale"></div>
+	<div id="mapMousePosition"></div>
+	<div id="mapProjection"></div>
+	<div id="mapOutput"></div>
+</div>
+<div style="clear:both;"></div>
+<!-- / map -->
