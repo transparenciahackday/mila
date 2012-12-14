2b4fe570a7aa025a6b5bbaf30033ff965a110c6e
Luís Lago <luisalveslago@gmail.com>
2012-12-14 00:36:41 +0000

    plugins para definir o estado do obstáculo

---
+++ plugins/incidentstatus/i18n/en_US/incident_status.php
@@ -0,0 +1,12 @@
+<?php
+	$lang = array(
+		'waiting'=>"Por resolver",
+		'taken_on'=>"A ser resolvido",
+		'resolved'=>"resolvido",
+		'waiting_span'=>"Verifique se o obstáculo está por resolver",
+		'taken_on_span'=>"Verifique se o obstáculo está a ser resolvido",
+		'resolved_span'=>"Verifique se o obstáculo foi resolvido",
+		'comment'=>"Comente a alteração de estado",
+		'incident_status'=>"Report status",
+	);
+?>
