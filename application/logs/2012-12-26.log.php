d4bbd4e15f9f864f282f72ea2355f288b41f3aeb
Luís Lago <luisalveslago@gmail.com>
2012-12-26 17:45:06 +0000

    traduções em ui_admin e ui_main

--- application/logs/2012-12-26.log.php
+++ application/logs/2012-12-26.log.php
@@ -1,3 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
 
 2012-12-26 15:17:26 +00:00 --- error: core.uncaught_exception
+2012-12-26 17:15:25 +00:00 --- error: core.uncaught_exception
